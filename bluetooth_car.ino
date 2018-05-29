6876d2cb683b930d84df4823a6addfad114ecf43
Sajith Gairuka <39628562+sajithgairuka@users.noreply.github.com>
2018-05-29 08:18:39 +0530

    Add files via upload

---
+++ bluetooth_car.ino
@@ -0,0 +1,38 @@
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+pinMode(5,OUTPUT);
+  pinMode(6,OUTPUT);
+  pinMode(9,OUTPUT);
+  pinMode(10,OUTPUT);
+}
+int spd=110;
+int spdd=255;
+void loop() {
+  // put your main code here, to run repeatedly:
+if(Serial.available()){
+  char c=Serial.read();
+  if(c=='1'){
+              analogWrite(5,spdd);
+                   analogWrite(10,spdd);
+    }
+    if(c=='2'){
+                   analogWrite(6,LOW);
+                   analogWrite(10,LOW); 
+                    analogWrite(5,LOW);
+                   analogWrite(9,LOW); 
+      }
+      if(c=='3'){
+         analogWrite(9,spd);
+                   analogWrite(5,spd); 
+        }
+        if(c=='4'){
+           analogWrite(6,spd);
+                   analogWrite(10,spd); 
+          }
+          if(c=='5'){
+             analogWrite(6,spdd);
+                    analogWrite(9,spdd); 
+            }
+  }
+}
---
+++ theft_detector.ino
@@ -0,0 +1,46 @@
+// include the library code:
+#include <LiquidCrystal.h>
+
+// initialize the library with the numbers of the interface pins
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+int Spin=8;//sensor pin out
+int LED=6;
+int cfl=7;//relay
+int pinSpeaker = 10;//Set up a speaker on a PWM pin (digital 9, 10, or 11)
+
+void setup() {
+ lcd.begin(16, 2);
+pinMode(Spin,INPUT);
+pinMode(LED,OUTPUT);
+pinMode(pinSpeaker, OUTPUT);
+pinMode(cfl, OUTPUT);
+}
+void loop() {
+int x=digitalRead(Spin);
+ lcd.setCursor(0,0);
+if(x==0){
+  digitalWrite(LED,LOW);
+  digitalWrite(cfl,HIGH);
+ playTone(0, 0);
+lcd.print("Secure !");
+  }
+  if(x==1){
+    digitalWrite(LED,HIGH);
+   digitalWrite(cfl,LOW);
+   playTone(200, 450);
+   lcd.print(" Detected Some! ");
+    }
+}
+void playTone(long duration, int freq) {
+    duration *= 1000;
+    int period = (1.0 / freq) * 1000000;
+    long elapsed_time = 0;
+    while (elapsed_time < duration) {
+        digitalWrite(pinSpeaker,HIGH);
+        delayMicroseconds(period / 2);
+        digitalWrite(pinSpeaker, LOW);
+        delayMicroseconds(period / 2);
+        elapsed_time += (period);
+    }
+}
